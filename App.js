747c068fb084f0c4a8ea9bc0983b1e7797146beb
MASISOMETAL <cristianmasisofarias@gmail.com>
2023-08-23 22:48:14 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,71 @@
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={{padding: 30}}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder='Item de la lista'
+                    placeholderTextColor={"#ff0000"}
+                    style={styles.input}
+                />
+                <Button title='ADD' color={"#ff0000"} />
+            </View>
+            <View style={styles.listContainer}>
+
+                <View style={styles.TextContainer}>
+                    <Text style={styles.Text}>Item 1</Text>
+                </View>
+
+                <View style={styles.TextContainer}>
+                    <Text style={styles.Text}>Item 2</Text>
+                </View>
+
+                <View style={styles.TextContainer}>
+                    <Text style={styles.Text}>Item 3</Text>
+                </View>
+
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 30
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    input: {
+        borderBottomColor: "#ff0000",
+        borderBottomWidth: 1,
+        width: 200,
+        height: 50,
+        color: "#ff0000",
+        fontSize: 20,
+        paddingLeft: 12,
+    },
+    listContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: "#ff0000",
+        borderWidth: 2,
+        marginTop: 40,
+    },
+    TextContainer: {
+        borderColor: "#ff0000",
+        alignItems: 'center',
+        borderWidth: 2,
+        marginVertical: 20,
+        padding: 10,
+        width: "90%",
+    },
+    Text: {
+        fontSize: 24,
+        color: "#ff0000",
+        fontWeight: 'bold',
+    }
+})
---
+++ src/component/Main.jsx
@@ -0,0 +1,12 @@
+import { View, Text} from "react"
+
+export default function Header() {
+    
+    return (
+        <View >
+            <Text>HOLA</Text>
+
+
+        </View>
+    );
+ }
